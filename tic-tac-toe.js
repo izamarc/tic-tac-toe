e4844dffbc2610a24fa25ed6aca15ed7f8d929dc
izamarc <Ca8098Iz1219@pusd.us>
2016-11-28 23:13:43 -0800

    DONE YAAYYY

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -20,8 +20,8 @@ whichplayer = 2
 }else if (whichplayer == 2){
   if(square1clicked == false){
  var myshape = document.createElementNS(namespace, "rect")
- rect.setAttribute("x", 75)
- rect.setAttribute("y", 50)
+ rect.setAttribute("x", 100)
+ rect.setAttribute("y", 55)
  rect.setAttribute("height", 20)
  rect.setAttribute("width",20)
  rect.setAttribute("fill","white")
@@ -60,7 +60,7 @@ else if (whichplayer ==2){
  var canvas = document.getElementById("game-board")
  canvas.appendChild(rect)
   square2clicked = true
- whichplayer = 2
+ whichplayer = 1
    }
  }
 }
@@ -90,7 +90,184 @@ whichplayer = 2
  var canvas = document.getElementById("game-board")
  canvas.appendChild(rect)
   square3clicked = true
+ whichplayer = 1
+   }
+ }
+}
+var square4clicked = false
+function square4(){
+  if(whichplayer == 1){
+    if(square4clicked == false){
+  var myShape = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", 45)
+  circle.setAttribute("cy", 45)
+  circle.setAttribute("r", 30)
+  circle.setAttribute("fill", "purple")
+  var  canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+square4clicked = true
+whichplayer = 2
+}
+}else if (whichplayer ==2){
+  if(square4clicked == false){
+ var myshape = document.createElementNS(namespace, "rect")
+ rect.setAttribute("x", 50)
+ rect.setAttribute("y", 50)
+ rect.setAttribute("height", 20)
+ rect.setAttribute("width",20)
+ rect.setAttribute("fill","white")
+ rect.setAttribute("opacity",1)
+ var canvas = document.getElementById("game-board")
+ canvas.appendChild(rect)
+  square4clicked = true
  whichplayer = 2
+   }
+ }
+}
+var square5clicked = false
+function square5(){
+  if(whichplayer == 1){
+    if(square5clicked == false){
+  var myShape = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", 45)
+  circle.setAttribute("cy", 45)
+  circle.setAttribute("r", 30)
+  circle.setAttribute("fill", "purple")
+  var  canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+square4clicked = true
+whichplayer = 2
+}
+}else if (whichplayer ==2){
+  if(square5clicked == false){
+ var myshape = document.createElementNS(namespace, "rect")
+ rect.setAttribute("x", 50)
+ rect.setAttribute("y", 50)
+ rect.setAttribute("height", 20)
+ rect.setAttribute("width",20)
+ rect.setAttribute("fill","white")
+ rect.setAttribute("opacity",1)
+ var canvas = document.getElementById("game-board")
+ canvas.appendChild(rect)
+  square5clicked = true
+ whichplayer = 1
+   }
+ }
+}
+var square6clicked = false
+function square6(){
+  if(whichplayer == 1){
+    if(square6clicked == false){
+  var myShape = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", 45)
+  circle.setAttribute("cy", 45)
+  circle.setAttribute("r", 30)
+  circle.setAttribute("fill", "purple")
+  var  canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+square6clicked = true
+whichplayer = 2
+}
+}else if (whichplayer ==2){
+  if(square6clicked == false){
+ var myshape = document.createElementNS(namespace, "rect")
+ rect.setAttribute("x", 50)
+ rect.setAttribute("y", 50)
+ rect.setAttribute("height", 20)
+ rect.setAttribute("width",20)
+ rect.setAttribute("fill","white")
+ rect.setAttribute("opacity",1)
+ var canvas = document.getElementById("game-board")
+ canvas.appendChild(rect)
+  square6clicked = true
+ whichplayer = 1
+   }
+ }
+}var square7clicked = false
+function square7(){
+  if(whichplayer == 1){
+    if(square7clicked == false){
+  var myShape = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", 45)
+  circle.setAttribute("cy", 45)
+  circle.setAttribute("r", 30)
+  circle.setAttribute("fill", "purple")
+  var  canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+square7clicked = true
+whichplayer = 2
+}
+}else if (whichplayer ==2){
+  if(square7clicked == false){
+ var myshape = document.createElementNS(namespace, "rect")
+ rect.setAttribute("x", 50)
+ rect.setAttribute("y", 50)
+ rect.setAttribute("height", 20)
+ rect.setAttribute("width",20)
+ rect.setAttribute("fill","white")
+ rect.setAttribute("opacity",1)
+ var canvas = document.getElementById("game-board")
+ canvas.appendChild(rect)
+  square7clicked = true
+ whichplayer = 1
+   }
+ }
+}var square8clicked = false
+function square8(){
+  if(whichplayer == 1){
+    if(square8clicked == false){
+  var myShape = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", 45)
+  circle.setAttribute("cy", 45)
+  circle.setAttribute("r", 30)
+  circle.setAttribute("fill", "purple")
+  var  canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+square8clicked = true
+whichplayer = 2
+}
+}else if (whichplayer ==2){
+  if(square8clicked == false){
+ var myshape = document.createElementNS(namespace, "rect")
+ rect.setAttribute("x", 50)
+ rect.setAttribute("y", 50)
+ rect.setAttribute("height", 20)
+ rect.setAttribute("width",20)
+ rect.setAttribute("fill","white")
+ rect.setAttribute("opacity",1)
+ var canvas = document.getElementById("game-board")
+ canvas.appendChild(rect)
+  square8clicked = true
+ whichplayer = 2
+   }
+ }
+}var square9clicked = false
+function square9(){
+  if(whichplayer == 1){
+    if(square9clicked == false){
+  var myShape = document.createElementNS(namespace, "circle")
+  circle.setAttribute("cx", 45)
+  circle.setAttribute("cy", 45)
+  circle.setAttribute("r", 30)
+  circle.setAttribute("fill", "purple")
+  var  canvas = document.getElementById("game-board")
+  canvas.appendChild(circle)
+square9clicked = true
+whichplayer = 2
+}
+}else if (whichplayer ==2){
+  if(square9clicked == false){
+ var myshape = document.createElementNS(namespace, "rect")
+ rect.setAttribute("x", 50)
+ rect.setAttribute("y", 50)
+ rect.setAttribute("height", 20)
+ rect.setAttribute("width",20)
+ rect.setAttribute("fill","white")
+ rect.setAttribute("opacity",1)
+ var canvas = document.getElementById("game-board")
+ canvas.appendChild(rect)
+  square9clicked = true
+ whichplayer = 1
    }
  }
 }
